a184ea7ac1b352bb79af6aacce87da64a5f3f2b2
Rafal Stachura <rafstach.97@gmail.com>
2024-01-22 19:21:49 +0100

    Initial commit for computer-game and doctor-office projects

---
+++ computer-game/src/game/Main.java
@@ -0,0 +1,20 @@
+package game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println(new Random().nextInt(-10, 10));
+//        try (Scanner scanner = new Scanner(System.in)) {
+//            int commandId = -1;
+//            do {
+//                commandId = scanner.nextInt();
+//
+//            } while (commandId != 0);
+//
+//        }
+    }
+
+}
